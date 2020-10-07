476fc80c4645cdfcd32662178322ac068988ba4b
antonyfleischer <64362907+antonyfleischer@users.noreply.github.com>
2020-10-07 22:37:28 +0200

    MoleculeAgent.py docstrings

--- MoleculeAgent.py
+++ MoleculeAgent.py
@@ -2,10 +2,37 @@ import gym
 from gym import wrappers, logger
 
 class MoleculeAgent:
+    """
+    A class used to represent an Agent that implements a RL policy.
+
+    ...
+    Methods
+    ----------
+    __init__(action_space)
+        the constructor method for a MoleculeAgent object.
+
+    act(observation, reward, done)
+        returns the chosen action based on the Agent's policy.
+    """
+
     def __init__(self, action_space):
+        """
+        Parameters
+        ----------
+        action_space : spaces.Discrete(6)
+            The actions available to the agent, represented numerically.
+        """
+
         self.action_space = action_space
   
     def act(self, observation, reward, done):
+        """Determines the MoleculeAgent's action to return to the environment.
+
+        Parameters
+        __________
+        observation : 
+
+        """
         #determine simply policy
         return self.action_space.sample()
 
